5a7916101027804c8a0344674f65be2b7f1ff648
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-11-25 23:10:45 +0300

    Adding textInputType property

--- lib/widgets/text_field_input.dart
+++ lib/widgets/text_field_input.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class TextFieldInput extends StatelessWidget {
   final String hintText;
   final bool isPassword;
+  final TextInputType textInputType;
 
   final TextEditingController textEditingController;
 
@@ -10,13 +11,15 @@ class TextFieldInput extends StatelessWidget {
       {super.key,
       required this.hintText,
       required this.textEditingController,
-      this.isPassword = false});
+      this.isPassword = false,
+      required this.textInputType});
 
   @override
   Widget build(BuildContext context) {
     final InputBorder inputBorder =
         OutlineInputBorder(borderSide: Divider.createBorderSide(context));
     return TextField(
+        keyboardType: textInputType,
         decoration: InputDecoration(
           // focusedBorder: ,
           // enabledBorder: ,
