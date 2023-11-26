248ca5d2b2a4d8412000b51af08c2960b9c7451a
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-11-26 23:31:03 +0300

    Using provider to access app state

--- lib/responsive/mobile_screen.dart
+++ lib/responsive/mobile_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/resources/resources_export.dart';
+import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Mobile Screen')));
+    return Scaffold(body: Center(child:
+        Consumer<UserProvider>(builder: (context, userProvider, complexChild) {
+      return Text(userProvider.getUser.username);
+    })));
   }
 }
