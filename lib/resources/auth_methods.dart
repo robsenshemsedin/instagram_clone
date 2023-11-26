967b341aff1e469f912021ad3af17b5f05138719
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-11-26 23:30:41 +0300

    Added logic for getting user detail and log in user

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,11 +1,20 @@
 import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone/models/models_export.dart' as model;
 import 'package:instagram_clone/resources/resources_export.dart';
 
 class AuthMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<model.User> getUserDetail() async {
+    final userId = _auth.currentUser?.uid;
+    final documentSnapshot =
+        await _firestore.collection('users').doc(userId).get();
+    final user = model.User.fromFirestore(documentSnapshot);
+    return user;
+  }
 
   Future<String> signUpUser({
     required String email,
@@ -14,7 +23,7 @@ class AuthMethods {
     required String bio,
     required Uint8List? imageData,
   }) async {
-    String res = "Some error Occurred";
+    String res = "Unkown error Occurred";
     try {
       if (email.isNotEmpty &&
           password.isNotEmpty &&
@@ -44,5 +53,21 @@ class AuthMethods {
       return err.toString();
     }
     return res;
+  }
+
+  Future<String> logInUser(
+      {required String email, required String password}) async {
+    String res = 'Unkown error occured';
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      res = 'success';
+    } catch (error) {
+      res = error.toString();
+    }
+    return res;
+  }
+
+  void signoutUser() {
+    _auth.signOut();
   }
 }
