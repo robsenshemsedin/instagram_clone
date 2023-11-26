dfac25fd0aaf4833e1eaf32a4b9747d2db1fd57f
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-11-26 23:29:37 +0300

    Using user identification data adding provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+// import 'package:instagram_clone/responsive/responsive_export.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:instagram_clone/firebase_options.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/responsive/responsive_export.dart';
 import 'package:instagram_clone/screens/screens_export.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const InstagramClone());
 }
 
@@ -13,13 +23,36 @@ class InstagramClone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData.dark()
-            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-        home: const LoginScreen()
-        // const ResponsiveLayout(
-        //   mobileScreenLayout: MobileScreenLayout(),
-        //   webScreenLayout: WebScreenLayout(),
-        // )
-        );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: mobileBackgroundColor,
+            progressIndicatorTheme:
+                const ProgressIndicatorThemeData(color: primaryColor)),
+        home: ChangeNotifierProvider(
+          create: (context) => UserProvider(),
+          child: StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: ((context, AsyncSnapshot<User?> snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  // If the data is not null then the user is logged in aready.
+                  if (snapshot.hasData) {
+                    return const ResponsiveLayout(
+                      mobileScreenLayout: MobileScreenLayout(),
+                      webScreenLayout: WebScreenLayout(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text(snapshot.error.toString()),
+                    );
+                  }
+
+                  // If the user data is null then the user is not logged in.
+                  else {
+                    return const LoginScreen();
+                  }
+                }
+                return const Center(child: CircularProgressIndicator());
+              })),
+        ));
   }
 }
