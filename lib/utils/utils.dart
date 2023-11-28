5cfadb5ee192cd52048ff4db46a554861eb53777
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-11-28 21:29:49 +0300

    Adding profile image picker method

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -16,4 +18,45 @@ showSnackBar(BuildContext context, String text) {
       content: Text(text),
     ),
   );
+}
+
+Future<Uint8List?> pickProfileImage(BuildContext parentContext) async {
+  const padding = EdgeInsets.all(20);
+  Uint8List? inerFile;
+  await showDialog(
+      context: parentContext,
+      builder: (context) {
+        return SimpleDialog(
+            title: const Text('Create a Post'),
+            children: <Widget>[
+              SimpleDialogOption(
+                  padding: padding,
+                  child: const Text('Take a photo'),
+                  onPressed: () async {
+                    Uint8List file = await pickImage(ImageSource.camera);
+                    inerFile = file;
+                    if (!context.mounted) return;
+
+                    Navigator.pop(context);
+                  }),
+              SimpleDialogOption(
+                  padding: padding,
+                  child: const Text('Choose from Gallery'),
+                  onPressed: () async {
+                    Uint8List file = await pickImage(ImageSource.gallery);
+                    inerFile = file;
+                    if (!context.mounted) return;
+
+                    Navigator.pop(context);
+                  }),
+              SimpleDialogOption(
+                padding: padding,
+                child: const Text("Cancel"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ]);
+      });
+  return inerFile;
 }
