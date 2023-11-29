7bde508c392ac3a6fdce4526e128f98e67443b9f
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-11-29 21:54:37 +0300

    Create comment_card.dart

---
+++ lib/widgets/comment_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CommentCard extends StatelessWidget {
+  final Map<String, dynamic> snapshot;
+  const CommentCard({
+    Key? key,
+    required this.snapshot,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              snapshot['profilePic'],
+            ),
+            radius: 18,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: snapshot['name'],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            )),
+                        TextSpan(
+                          text: ' ${snapshot['text']}',
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Text(
+                      DateFormat.yMMMd().format(
+                        snapshot['datePublished'].toDate(),
+                      ),
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Icon(
+              Icons.favorite,
+              size: 16,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
