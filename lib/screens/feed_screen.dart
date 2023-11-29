9f92f310d9d6f9ff0198d85934682dc991786cd7
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-11-29 21:53:43 +0300

    Making the page responsive

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -14,36 +14,50 @@ class FeedScreen extends StatefulWidget {
 class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          centerTitle: false,
-          backgroundColor: Colors.black,
-          title: SvgPicture.asset(
-            'assets/ic_instagram.svg',
-            colorFilter: const ColorFilter.mode(primaryColor, BlendMode.srcIn),
-            height: 32,
-          ),
-          actions: [
-            IconButton(
-              icon: const Icon(
-                Icons.messenger_outline,
-                color: primaryColor,
+        backgroundColor:
+            width > webScreenSize ? webBackgroundColor : mobileBackgroundColor,
+        appBar: width > webScreenSize
+            ? null
+            : AppBar(
+                centerTitle: false,
+                backgroundColor: Colors.black,
+                title: SvgPicture.asset(
+                  'assets/ic_instagram.svg',
+                  colorFilter:
+                      const ColorFilter.mode(primaryColor, BlendMode.srcIn),
+                  height: 32,
+                ),
+                actions: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.messenger_outline,
+                      color: primaryColor,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
               ),
-              onPressed: () {},
-            ),
-          ],
-        ),
         body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-            stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+            stream: FirebaseFirestore.instance
+                .collection('posts')
+                .orderBy('datePublished', descending: true)
+                .snapshots(),
             builder: (context,
                 AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
               if (snapshot.connectionState == ConnectionState.active) {
                 if (snapshot.hasData) {
                   return ListView.builder(
                     itemBuilder: (context, index) {
-                      return PostCard(
-                        snapshot: snapshot.data!.docs[index].data(),
+                      return Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: width > webScreenSize ? width * 0.3 : 0,
+                          vertical: width > webScreenSize ? 15 : 0,
+                        ),
+                        child: PostCard(
+                          snapshot: snapshot.data!.docs[index].data(),
+                        ),
                       );
                     },
                     itemCount: snapshot.data!.docs.length,
